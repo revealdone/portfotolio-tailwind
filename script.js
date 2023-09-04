1d5e8da2061f083a22cfed19ed46eb4617a7cf8f
revealdone <muhamadwahidromdoni@gmail.com>
2023-09-04 16:02:41 +0700

    Pesan commit Anda di sini

---
+++ script.js
@@ -0,0 +1,45 @@
+function toggleVideo() {
+  const trailer = document.querySelector(".trailer");
+  const video = document.querySelector("video");
+  video.pause();
+  trailer.classList.toggle("active");
+}
+
+function changeBg(bg, title) {
+  const banner = document.querySelector(".banner");
+  const contents = document.querySelectorAll(".content");
+  banner.style.background = `url("./img/${bg}")`;
+  banner.style.backgroundSize = "cover";
+  banner.style.backgroundPosition = "center";
+
+  contents.forEach((content) => {
+    content.classList.remove("active");
+    if (content.classList.contains(title)) {
+      content.classList.add("active");
+    }
+  });
+}
+
+// sdasdas
+// asdasdas
+
+//navbar fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.scrollY > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+//hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
