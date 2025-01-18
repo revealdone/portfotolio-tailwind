192af95e39a762df6421dd78a938d943f5699303
revealdone <muhamadwahidromdoni@gmail.com>
2025-01-18 10:29:43 +0700

    Pesan commit Anda di sini

--- dist/js/script.js
+++ dist/js/script.js
@@ -29,11 +29,16 @@ $(document).ready(function () {
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector('#to-top');
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove('hidden');
+    toTop.classList.add('flex');
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove('flex');
+    toTop.classList.add('hidden');
   }
 };
 
@@ -44,6 +49,14 @@ const navMenu = document.querySelector("#nav-menu");
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
+});
+
+// klik di luar hamburger
+window.addEventListener('click', function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+  navMenu.classList.add("hidden");
+  }
 });
 
 var myIndex = 0;
